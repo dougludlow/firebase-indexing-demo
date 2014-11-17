b12ef35b39112404cb45b96fce61e5d7547bf346
Douglas Ludlow <doug.ludlow@gmail.com>
2014-11-16 17:35:25 -0700

    Update about.

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,10 +8,6 @@
  * Controller of the indexingApp
  */
 angular.module('indexingApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AboutCtrl', function () {
+
   });
