4fca76cfbd73190d77dc09399d85badad6eeeb69
Douglas Ludlow <doug.ludlow@gmail.com>
2014-11-16 17:35:47 -0700

    Update main.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,31 @@
  * # MainCtrl
  * Controller of the indexingApp
  */
-angular.module('indexingApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular
+  .module('indexingApp')
+  .controller('MainCtrl', Main);
+
+function Main($scope, indexing) {
+  var vm = this;
+
+  vm.record = record;
+  activate();
+
+  function activate() {
+    indexing.$bindTo($scope, 'vm.indexing');
+  }
+
+  function record() {
+    var batches = parseInt(vm.batches);
+
+    if (!isNaN(batches)) {
+
+      if (!vm.indexing.totalBatches) {
+        vm.indexing.totalBatches = 0;
+      }
+
+      vm.batches = '';
+      vm.indexing.totalBatches += batches;
+    }
+  }
+}
