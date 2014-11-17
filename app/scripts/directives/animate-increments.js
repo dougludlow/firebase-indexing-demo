92cc0b77dfa46936d607ba20988f84d37673e93f
Douglas Ludlow <doug.ludlow@gmail.com>
2014-11-16 17:34:27 -0700

    Add animated counting directive.

---
+++ app/scripts/directives/animate-increments.js
@@ -0,0 +1,58 @@
+'use strict';
+
+angular
+  .module('indexingApp')
+  .directive('animateIncrements', Incrementer);
+
+function Incrementer() {
+  return {
+    restrict: 'A',
+    scope: {
+      current: '&animateIncrements'
+    },
+    template: '{{value}}',
+    controller: IncrementCtrl
+      //link: link
+  };
+
+  // function link(scope, element, attr) {
+  //
+  // }
+
+  function IncrementCtrl($scope, $interval) {
+
+    $scope.value = 0;
+
+    $scope.$watch('current()', function(current, old) {
+      animateChange(old, current, 50);
+    });
+
+    function animateChange(start, end, duration) {
+      start = start || 0;
+      end = end || 0;
+
+      if (start !== end) {
+
+        var range = end - start;
+        var current = start;
+        var increment = 1;
+        var step = Math.abs(Math.floor(duration / range));
+
+        var stop = $interval(function() {
+
+          if (end > start) {
+            current += increment;
+          } else {
+            current -= increment;
+          }
+
+          $scope.value = current;
+
+          if (current === end) {
+            $interval.cancel(stop);
+          }
+        }, step);
+      }
+    }
+  }
+}
