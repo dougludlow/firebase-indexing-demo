063693f5802ccfc9bb3c21df03d236a7e2eb2773
Douglas Ludlow <doug.ludlow@gmail.com>
2014-11-23 22:48:03 -0700

    Add undo.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,12 +11,13 @@ angular
   .module('indexingApp')
   .controller('MainCtrl', Main);
 
-function Main($scope, dataservice) {
+function Main($scope, $timeout, dataservice) {
   var vm = this;
   var ds = dataservice($scope, 'vm.indexing');
 
   vm.record = record;
   vm.total = ds.total;
+  vm.undo = undo;
 
   function record() {
     var batches = parseInt(vm.batches);
@@ -24,6 +25,17 @@ function Main($scope, dataservice) {
     if (!isNaN(batches)) {
       ds.record(batches);
       vm.batches = '';
+      vm.successBatches = batches;
+      vm.undoVisible = false;
+
+      $timeout(function(){
+        vm.undoVisible = true;
+      });
     }
+  }
+
+  function undo() {
+    vm.undoVisible = false;
+    ds.undo();
   }
 }
